46abe053d17f8b4dd399e55637cf4ff788051d9c
Paul Williams <paul@paulswilliams.me.uk>
2019-10-22 21:12:56 +0100

    Ids properly support value semantics

--- Marketplace.Domain/UserId.cs
+++ Marketplace.Domain/UserId.cs
@@ -1,18 +1,11 @@
 using System;
+using Marketplace.Framework;
 
 namespace Marketplace.Domain
 {
-    public class UserId
+    public class UserId : Value<UserId>
     {
         private readonly Guid _value;
-
-        public UserId(Guid value)
-        {
-            if (value == default)
-            {
-                throw new ArgumentNullException(nameof(value), "User id cannot be empty");
-            }
-            _value = value;
-        }
+        public UserId(Guid value) => _value = value;
     }
 }
