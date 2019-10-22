28bc305982c6e0c02cfba1c08eb15d6e480c2bde
Paul Williams <paul@paulswilliams.me.uk>
2019-10-22 21:08:43 +0100

    Price value type, and Money supports immutable addition and subtraction

--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using Marketplace.Domain;
 using Xunit;
 
@@ -11,6 +12,22 @@ namespace Marketplace.Tests
             var firstAmount = new Money(5);
             var secondAmount = new Money(5);
             Assert.Equal(firstAmount, secondAmount);
+        }
+
+        [Fact]
+        public void Sum_of_money_gives_full_amount()
+        {
+            var coin_1 = new Money(1);
+            var coin_2 = new Money(2);
+            var coin_3 = new Money(2);
+            Money bank_note = new Money(5);
+            Assert.Equal(bank_note, coin_1 + coin_2 + coin_3);
+        }
+
+        [Fact]
+        public void Difference_of_money_gives_difference()
+        {
+            Assert.Equal(new Money(1), new Money(3) - new Money(2));
         }
     }
 }
