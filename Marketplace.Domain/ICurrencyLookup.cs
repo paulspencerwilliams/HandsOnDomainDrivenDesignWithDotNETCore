6a37106ba1da7bdda8345fd1d2439c3dfbeffac8
Paul Williams <paul@paulswilliams.me.uk>
2019-10-23 17:18:28 +0100

    Interface for currency lookup domain service

---
+++ Marketplace.Domain/ICurrencyLookup.cs
@@ -0,0 +1,20 @@
+using System.Data.SqlTypes;
+using System.Security.Cryptography.X509Certificates;
+using Marketplace.Framework;
+
+namespace Marketplace.Domain
+{
+    public interface ICurrencyLookup
+    {
+        Currency FindCurrency(string currencyCode);
+    }
+
+    public class Currency : Value<Currency>
+    {
+        public string CurrencyCode { get; set; }
+        public bool InUse { get; set; }
+        public int DecimalPlaces { get; set; }
+        
+        public static Currency None = new Currency {InUse = false};
+    }
+}
