d3797be41cb1ac1bee7f1f90c592362ad673fdc9
Paul Williams <paul@paulswilliams.me.uk>
2019-10-23 06:55:21 +0100

    Partial implementation of currency with no validation of currency code

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -6,28 +6,58 @@ namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
-        public static Money FromDecimal(decimal amount) => new Money(amount);
-        
-        public static Money FromString(String amount) => new Money(decimal.Parse(amount));
+        private const string DefaultCurrency = "EUR";
+
+        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) =>
+            new Money(amount, currency);
+
+        public static Money FromString(String amount, string currency = DefaultCurrency) =>
+            new Money(decimal.Parse(amount), currency);
+
         public decimal Amount { get; }
+        public string CurrencyCode { get; }
 
-        protected Money(decimal amount)
+        protected Money(decimal amount, string currencyCode = "EUR")
         {
-
             if (decimal.Round(amount, 2) != amount)
             {
-                throw new ArgumentOutOfRangeException(nameof(amount), "Ammount cannot have more than two decimals");
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount cannot have more than two decimals");
             }
 
             Amount = amount;
+            CurrencyCode = currencyCode;
         }
 
-        public Money Add(Money summand) => new Money(Amount + summand.Amount);
 
-        public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+        public Money Add(Money summand)
+        {
+            if (CurrencyCode != summand.CurrencyCode)
+            {
+                throw new CurrencyMismatchException("Cannot sum amounts with different currencies");
+            }
+
+            return new Money(Amount + summand.Amount);
+        }
+
+        public Money Subtract(Money subtrahend)
+        {
+            if (CurrencyCode != subtrahend.CurrencyCode)
+            {
+                throw new CurrencyMismatchException("Cannot subtract amounts with different currencies");
+            }
+
+            return new Money(Amount - subtrahend.Amount);
+        }
 
         public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
-        
+
         public static Money operator -(Money subtrahend1, Money subtrahend2) => subtrahend1.Subtract(subtrahend2);
+    }
+
+    public class CurrencyMismatchException : Exception
+    {
+        public CurrencyMismatchException(string message) : base(message)
+        {
+        }
     }
 }
