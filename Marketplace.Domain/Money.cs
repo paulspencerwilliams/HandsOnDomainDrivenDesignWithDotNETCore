ef10d0db5efd204134ad31ada30b8db2202b94ad
Paul Williams <paul@paulswilliams.me.uk>
2019-10-23 06:41:21 +0100

    Added factories to Money value type

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -1,13 +1,27 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
 using Marketplace.Framework;
 
 namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
+        public static Money FromDecimal(decimal amount) => new Money(amount);
+        
+        public static Money FromString(String amount) => new Money(decimal.Parse(amount));
         public decimal Amount { get; }
-        
-        public Money(decimal amount) => Amount = amount;
-        
+
+        protected Money(decimal amount)
+        {
+
+            if (decimal.Round(amount, 2) != amount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Ammount cannot have more than two decimals");
+            }
+
+            Amount = amount;
+        }
+
         public Money Add(Money summand) => new Money(Amount + summand.Amount);
 
         public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -9,25 +9,25 @@ namespace Marketplace.Tests
         [Fact]
         public void Money_objects_with_the_same_amount_should_be_equal()
         {
-            var firstAmount = new Money(5);
-            var secondAmount = new Money(5);
+            var firstAmount = Money.FromDecimal(5);
+            var secondAmount = Money.FromDecimal(5);
             Assert.Equal(firstAmount, secondAmount);
         }
 
         [Fact]
         public void Sum_of_money_gives_full_amount()
         {
-            var coin_1 = new Money(1);
-            var coin_2 = new Money(2);
-            var coin_3 = new Money(2);
-            Money bank_note = new Money(5);
+            var coin_1 = Money.FromDecimal(1);
+            var coin_2 = Money.FromDecimal(2);
+            var coin_3 = Money.FromDecimal(2);
+            Money bank_note = Money.FromDecimal(5);
             Assert.Equal(bank_note, coin_1 + coin_2 + coin_3);
         }
 
         [Fact]
         public void Difference_of_money_gives_difference()
         {
-            Assert.Equal(new Money(1), new Money(3) - new Money(2));
+            Assert.Equal(Money.FromDecimal(1), Money.FromDecimal(3) - Money.FromDecimal(2));
         }
     }
 }
