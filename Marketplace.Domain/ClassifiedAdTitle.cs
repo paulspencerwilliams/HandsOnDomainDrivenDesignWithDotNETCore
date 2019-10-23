cd4e98342af99b3e8bd67b9d4b84015d17351cd6
Paul Williams <paul@paulswilliams.me.uk>
2019-10-23 06:41:09 +0100

    ClassifiedAdTitle value created

---
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text.RegularExpressions;
+using Marketplace.Framework;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAdTitle : Value<ClassifiedAdTitle>
+    {
+        public static ClassifiedAdTitle FromString(string title) => new ClassifiedAdTitle(title);
+
+        public static ClassifiedAdTitle FromHtml(string htmlTitle)
+        {
+            var supportedTagsReplaced = htmlTitle
+                .Replace("<i>", "*")
+                .Replace("</i>", "*")
+                .Replace("<b>", "*")
+                .Replace("</b>", "*");
+            return new ClassifiedAdTitle(Regex.Replace(supportedTagsReplaced, "<.*?>", String.Empty));
+        }
+
+        private readonly string _value;
+
+        private ClassifiedAdTitle(string value)
+        {
+            if (value.Length > 100)
+            {
+                throw new ArgumentOutOfRangeException("Title cannot be longer than 100 characters", nameof(value));
+            }
+            _value = value;
+        }
+    }
+}
