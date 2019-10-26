bd78a555932310684f0a83821f3de6b8cc800e27
Paul Williams <paul@paulswilliams.me.uk>
2019-10-26 16:26:53 +0100

    Promote ClassifiedAd to aggregate root using new base class

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -4,17 +4,16 @@ using Marketplace.Tests;
 
 namespace Marketplace.Domain
 {
-    public class ClassifiedAd : Entity<ClassifiedAdId>
-
+    public class ClassifiedAd : AggregateRoot<ClassifiedAdId>
     {
         public ClassifiedAdId Id { get; private set; }
-        public ClassifiedAdState State { get; private set; }
-
         public UserId OwnerId { get; private set; }
         public ClassifiedAdTitle Title { get; private set; }
         public ClassifiedAdText Text { get; private set; }
         public Price Price { get; private set; }
-
+        public ClassifiedAdState State { get; private set; }
+        public UserId ApprovedBy { get; private set; }
+        
         public ClassifiedAd(ClassifiedAdId id, UserId ownerId)
         {
             Apply(new Events.ClassifiedAdCreated
@@ -105,8 +104,6 @@ namespace Marketplace.Domain
                 throw new InvalidEntityStateException(this, $"Post-checks failed in state {State}");
             }
         }
-
-        public UserId ApprovedBy { get; private set; }
 
         public enum ClassifiedAdState
         {
--- Marketplace.Framework/Entity.cs
+++ Marketplace.Framework/Entity.cs
@@ -1,29 +1,8 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
 namespace Marketplace.Framework
 {
     public abstract class Entity<TId> where TId : IEquatable<TId>
     {
-        private readonly List<object> _events;
-
-        protected Entity() => _events = new List<object>();
-
-        protected void Apply(object @event)
-        {
-            When(@event);
-            EnsureValidState();
-            _events.Add(@event);
-        }
-
-        protected abstract void When(object @event);
-
-        public IEnumerable<object> GetChanges() => _events.AsEnumerable();
-
-        public void ClearChanges() => _events.Clear();
-        protected abstract void EnsureValidState();
     }
 }
